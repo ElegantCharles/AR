68dff26033428b1dfb88d73aefb730e46994ccc6
ElegantCharles <echeverriamoscoso0@icloud.com>
2025-07-04 18:32:06 -0400

    Configuro MEDIA_ROOT

--- yukiAR/settings.py
+++ yukiAR/settings.py
@@ -76,5 +76,11 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# media
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+    MEDIA_ROOT,  
+]
